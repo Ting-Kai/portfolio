6ecf190c1a7d71bd24687a5d849de87184401031
wickedpapaya <tkch9973@gmail.com>
2021-01-04 14:31:32 +0800

    0104 updata

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
-$("#right-side-line-js").height(0);
-$(function () {
-    $(window).scroll(function () {
-      var scrollVal = $(this).scrollTop();
-      $("#right-side-line-js").height(scrollVal);
-    });
-});
+// $("#right-side-line-js").height(0);
+// $(function () {
+//     $(window).scroll(function () {
+//       var scrollVal = $(this).scrollTop();
+//       $("#right-side-line-js").height(scrollVal);
+//     });
+// });
